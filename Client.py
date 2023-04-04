fa6430df221fefbd50988783279d7bccedfc61c4
0X54nj4Y_j4nGr4 <106859771+Jatin-00189@users.noreply.github.com>
2023-04-03 23:35:27 -1200

    Create Client.py

---
+++ Client.py
@@ -0,0 +1,23 @@
+import socket
+
+client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+client.connect(("127.0.0.1",4441))
+
+while True:
+
+    data = input("Enter here anything to send server : ")
+
+    client.send(data.encode())
+
+    server_data = client.recv(4096)
+    if(server_data == ''): break
+    if(data == 'bye'): break
+
+    print("[+] Server sent you :  ",server_data.decode())
+
+client.close()
+print("kat Gya")
+
+
+
