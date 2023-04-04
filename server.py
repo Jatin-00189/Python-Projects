2bdf76135839880bb6d0dd4587ac384ee6648d58
0X54nj4Y_j4nGr4 <106859771+Jatin-00189@users.noreply.github.com>
2023-04-03 23:35:01 -1200

    Create server.py

---
+++ server.py
@@ -0,0 +1,25 @@
+import socket
+
+server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+server_socket.bind(("127.0.0.1",4441))
+
+server_socket.listen(10)
+
+print("Server is listning here 127.0.0.1:9000........")
+
+while True:
+
+    connection,address = server_socket.accept()
+
+    print("Ban Gya")
+
+    while True:
+
+        data = connection.recv(1024)
+        if(data.decode() == 'bye'): break
+        print("[+] Client says :",data.decode())
+        server_data = input("Enter to send client : ")
+        connection.send(server_data.encode())
+
+    connection.close()
